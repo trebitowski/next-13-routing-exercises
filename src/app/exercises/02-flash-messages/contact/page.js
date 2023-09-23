7215ffb42b59167410c4d849347efd123fd07a1b
Tyler Trebitowski <tyler@investii.com>
2023-09-23 15:29:26 -0500

    flash messages exercise

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,30 @@
-'use client';
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+import { ToastContext } from "../../../../components/ToastProvider";
+
+const REDIRECT_URL = "/exercises/02-flash-messages";
 
 function ContactPage() {
+  const router = useRouter();
+
+  const { createToast } = React.useContext(ToastContext);
+
+  function handlerSubmit(event) {
+    event.preventDefault();
+
+    router.push(REDIRECT_URL);
+
+    createToast(
+      "Your message was received. We'll get back to you shortly!",
+      "success"
+    );
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handlerSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
